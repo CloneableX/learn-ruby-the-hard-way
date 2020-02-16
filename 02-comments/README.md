c3b6cba7c1028665d1540312bd72d173afd52f4d
Cloneable <cloneable@163.com>
2020-02-16 17:38:47 +0000

    exercise 02: comments

---
+++ 02-comments/README.md
@@ -0,0 +1,3 @@
+# Exercise 02: Comments
+
+可以通过 `#` 在 ruby 源码中增加注释，或者临时性地屏蔽代码
---
+++ 02-comments/comments.rb
@@ -0,0 +1,8 @@
+# comment
+# this is ignored by ruby
+
+puts "It's also be good" # comment is also after code
+
+# puts "it can't run"
+
+puts "It's running"
