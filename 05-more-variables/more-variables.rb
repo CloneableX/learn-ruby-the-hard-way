8fda9ac3c6c53637aedbae0a58a9a099e7df6795
Cloneable <cloneable@163.com>
2020-02-18 08:49:52 +0000

    exercise 05: more variables and printing

---
+++ 05-more-variables/more-variables.rb
@@ -0,0 +1,20 @@
+my_name = "Cloneable"
+my_age = 22
+my_height = 70.0
+my_weight = 180.0
+my_eyes = "Black"
+my_teeth = "White"
+my_hair = "Brown"
+my_height_cm = my_height * 2.54
+my_weight_kg = my_weight * 0.454
+
+puts "Let's talk about #{my_name}"
+puts "He is #{my_age} years old"
+puts "He's #{my_height} inches height"
+puts "He's #{my_height_cm} cm height"
+puts "He's #{my_weight} pounds weight"
+puts "He's #{my_weight_kg} kg weight"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair"
+puts "His teeth are usually #{my_teeth} depending on the coffee"
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
