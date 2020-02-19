8f0a9adadbc290c7992efca1662a7dcb3d055583
Cloneable <cloneable@163.com>
2020-02-19 22:20:09 +0000

    exercise 10: what was that

---
+++ 10-what-was-that/README.md
@@ -0,0 +1,3 @@
+# Exercise 10: What was that
+
+在 ruby 中除了之前使用的 `%q{}` 之外还可以使用 `"""` 来包含多行字符串
---
+++ 10-what-was-that/what-was-that.rb
@@ -0,0 +1,16 @@
+tab_cat = "\tI'm tabbed in."
+split_line_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list:
+\t * Cat food
+\t * Fishies
+\t * Catnip
+\t * Grass
+"""
+
+puts tab_cat
+puts split_line_cat
+puts backslash_cat
+puts fat_cat
