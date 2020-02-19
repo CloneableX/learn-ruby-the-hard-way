30844ff449124123a7b6e066d4ecd6efad5dd772
Cloneable <cloneable@163.com>
2020-02-19 21:43:38 +0000

    exercise 09: print print print

---
+++ 09-print-print-print/README.md
@@ -0,0 +1,3 @@
+# Exercise 09: print, print, print
+
+在 ruby 中可以使用 `%q{}` 括上一段多行的字符串，这段字符串可以全体打印或者整体赋值给变量
---
+++ 09-print-print-print/print-print-print.rb
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\n"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+my_words = %q{
+  There's something going on here.
+  With this weird quote
+  We will be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+}
+
+puts my_words
