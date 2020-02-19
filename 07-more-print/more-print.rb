68d8b987d82cfbdf8e04da19d73bd381247946fd
Cloneable <cloneable@163.com>
2020-02-19 11:16:19 +0000

    exercise 07: more print

---
+++ 07-more-print/more-print.rb
@@ -0,0 +1,21 @@
+puts "Mary has a little lamb"
+puts "Its fleece was white as #{'snow'}"
+puts "And everywhere that Mary went"
+
+puts "*" * 10
+
+word1 = "C"
+word2 = "h"
+word3 = "e"
+word4 = "e"
+word5 = "s"
+word6 = "e"
+word7 = "B"
+word8 = "u"
+word9 = "r"
+word10 = "g"
+word11 = "e"
+word12 = "r"
+
+print word1 + word2 + word3 + word4 + word5 + word6
+puts word7 + word8 + word9 + word10 + word11 + word12
